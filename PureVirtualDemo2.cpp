a40e694c6abbd372f635bccf6c7efc28bfc93ee7
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2025-12-09 10:23:35 +0530

    Add files via upload

---
+++ PureVirtualDemo2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class Base
+{
+    public:
+       int i,j;
+
+       int Addition(int a, int b)       // concrete
+       {
+           return a+b;
+       }
+        virtual int Substraction(int a, int b) = 0;       // abstract
+
+};
+
+#pragma pack(1)
+class Derived : public Base                 
+{
+    public:
+       int x;
+
+       int Substraction(int a, int b)       // concrete
+       {
+            return a-b;
+       }
+       int Multiplication(int a, int b)       // concrete
+       {
+            return a*b;
+       }       
+};
+
+int main()
+{
+    Base * bp = new Derived();                 // up casting
+    int iRet = 0;
+
+    iRet = bp->Addition(11,10);         // 21
+    cout<<iRet<<"\n";
+
+    iRet = bp->Substraction(10,11);    // 1
+    cout<<iRet<<"\n";
+    
+   // iRet = bp->Multiplication(11,10);    Error
+
+    return 0;
+}
