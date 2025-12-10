b19c40e0a55fb68fbfb01ba57963522d3f194d09
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2025-12-10 11:19:37 +0530

    Add files via upload

---
+++ VirtualDemo4.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+       int i,j;
+
+       void fun()
+       {
+           cout<<"Inside Base fun \n";
+       }
+
+       void gun()
+       {
+           cout<<"Inside Base gun \n";
+       }
+
+       virtual void sun()
+       {
+           cout<<"Inside Base sun \n";
+       }
+
+       virtual void bun()
+       {
+           cout<<"Inside Base bun \n";
+       }
+
+
+};
+
+class Derived : public Base
+{
+    public:
+       int x;
+
+       void gun()
+       {
+           cout<<"Inside Derived gun \n";
+       }
+
+        void sun()
+       {
+           cout<<"Inside Derived usn \n";
+       }
+
+        void run()
+       {
+           cout<<"Inside Derived run \n";
+       }
+
+        void mun()
+       {
+           cout<<"Inside Derived mun \n";
+       }
+};
+
+int main()
+{
+    cout<<sizeof(Base)<<"\n";                 // 16
+    cout<<sizeof(Derived)<<"\n";             // 20
+
+    Base * bp = new Derived();                 // up casting
+
+    /*bp->fun();                                // Base fun
+    bp->gun();                                // Base gun
+    bp->sun();                                // Base sun
+    // bp->run();                                // error
+    // bp->mun();                                // error
+    bp->bun();                                // Base bun*/
+
+
+    return 0;
+}
